ab5efdbb93e18a7b3f30bb29f5c9128799b0bb50
taisiia-filikova <filikova@gmail.com>
2020-12-01 17:19:05 +0200

    Создает и стилизирует компонент Статистика

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import Container from './components/Container/Container';
+
 import Profile from './components/Profile/Profile';
 import user from './data/user.json';
+
+import Statistics from './components/Statistics/Statistics';
+import statisticalData from './data/statistical-data.json';
+
 import './App.css';
 
 function App() {
@@ -13,6 +18,7 @@ function App() {
         avatar={user.avatar}
         stats={user.stats}
       />
+      <Statistics title="Upload stats" stats={statisticalData} />
     </Container>
   );
 }
