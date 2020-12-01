3cd54364bb409454d28e84bb71b251060b042843
taisiia-filikova <filikova@gmail.com>
2020-12-01 18:16:06 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ import statisticalData from './data/statistical-data.json';
 import FriendList from './components/FriendList/FriendList';
 import friends from './data/friends.json';
 
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
+import transactions from './data/transactions.json';
+
 import './App.css';
 
 function App() {
@@ -23,35 +26,9 @@ function App() {
       />
       <Statistics title="Upload stats" stats={statisticalData} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </Container>
   );
 }
 
 export default App;
-
-// import Container from './components/Container/Container';
-// import Profile from './components/Profile/Profile';
-// import Statistics from './components/Statistics/Statistics';
-// import FriendList from './components/FriendList/FriendList';
-// import TransactionHistory from './components/TransactionHistory/TransactionHistory';
-// import user from './data/user.json';
-// import statisticalData from './data/statistical-data.json';
-// import friends from './data/friends.json';
-// import transactions from './data/transactions.json';
-
-// function App() {
-//   return (
-//     <Container>
-//       <Profile
-//         name={user.name}
-//         tag={user.tag}
-//         location={user.location}
-//         avatar={user.avatar}
-//         stats={user.stats}
-//       />
-//       <Statistics title="Upload stats" stats={statisticalData} />
-//       <FriendList friends={friends} />
-//       <TransactionHistory items={transactions} />
-//     </Container>
-//   );
-// }
