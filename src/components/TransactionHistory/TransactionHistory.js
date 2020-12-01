043dc7340daecb85cdc43ae7677e1dc75a7268f9
taisiia-filikova <filikova@gmail.com>
2020-12-01 19:02:46 +0200

    Добавляет и стилизирует компонент История Транзакций

---
+++ src/components/TransactionHistory/TransactionHistory.js
@@ -0,0 +1,38 @@
+import PropTypes from 'prop-types';
+import TransactionHistoryItem from './TransactionHistoryItem';
+import s from './TransactionHistory.module.css';
+
+function TransactionHistory({ items }) {
+  return (
+    <table className={s.history}>
+      <thead className={s.title}>
+        <tr>
+          <th className={s.cell}>Type</th>
+          <th className={s.cell}>Amount</th>
+          <th className={s.cell}>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {items.map(({ id, type, amount, currency }) => (
+          <TransactionHistoryItem
+            key={id}
+            type={type}
+            amount={amount}
+            currency={currency}
+          />
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+TransactionHistory.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    }),
+  ),
+};
+
+export default TransactionHistory;
---
+++ src/components/TransactionHistory/TransactionHistoryItem.js
@@ -0,0 +1,20 @@
+import PropTypes from 'prop-types';
+import s from './TransactionHistoryItem.module.css';
+
+function TransactionHistoryItem({ type, amount, currency }) {
+  return (
+    <tr className={s.row}>
+      <td className={s.cell}>{type}</td>
+      <td className={s.cell}>{amount}</td>
+      <td className={s.cell}>{currency}</td>
+    </tr>
+  );
+}
+
+TransactionHistoryItem.propTypes = {
+  type: PropTypes.string,
+  amount: PropTypes.number,
+  currency: PropTypes.string,
+};
+
+export default TransactionHistoryItem;
